acd291491b576bc1f8e3389456f725ae223aa05f
Koranech <antrusnakenko@gmail.com>
2026-06-29 15:31:06 +0300

    протипізував сервіси

--- app/services/api.service.ts
+++ app/services/api.service.ts
@@ -1,9 +1,11 @@
 import {IUser} from "@/app/models/IUser";
+import {IPost} from "@/app/models/IPost";
+import {IComment} from "@/app/models/IComment";
 
 const baseUrl = 'https://jsonplaceholder.typicode.com'
 
 export const apiService = {
-    getUsers: async () => {
+    getUsers: async (): Promise<IUser[]> => {
         return await fetch(baseUrl + '/users')
             .then(res => res.json())
     },
@@ -11,19 +13,19 @@ export const apiService = {
         return await fetch(baseUrl + '/users/' + id)
             .then(res => res.json())
     },
-    getPosts: async () => {
+    getPosts: async (): Promise<IPost[]> => {
         return await fetch(baseUrl + '/posts')
             .then(res => res.json())
     },
-    getPostById: async (id: string) => {
+    getPostById: async (id: string): Promise<IPost> => {
         return await fetch(baseUrl + '/posts/' + id)
         .then(res => res.json())
     },
-    getComments: async () => {
+    getComments: async ():Promise<IComment[]> => {
         return await fetch(baseUrl + '/comments')
             .then(res => res.json())
     },
-    getCommentById: async (id: string) => {
+    getCommentById: async (id: string): Promise<IComment> => {
         return await fetch(baseUrl + '/comments/' + id)
         .then(res => res.json())
     }
